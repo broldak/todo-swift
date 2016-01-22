40a459ff43039991e1a9513d3c827786a5413c0a
Brian Oldak <boldak13@gmail.com>
2016-01-22 11:20:11 -0800

    initial commit

---
+++ NewTodoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewTodoViewController.swift
+//  
+//
+//  Created by Brian Oldak on 1/22/16.
+//
+//
+
+import UIKit
+
+class NewTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
