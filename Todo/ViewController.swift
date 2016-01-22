531ca45313db8c8ed9e3ec3989546444c53cc226
Brian Oldak <boldak13@gmail.com>
2016-01-22 10:44:44 -0800

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Brian Oldak on 1/22/16.
+//  Copyright (c) 2016 Oldak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
